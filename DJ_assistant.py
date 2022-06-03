8c8864aff6030d1faa2b9b33c54fd8bedd93cb19
JohanHorsmans <horsmans1004@gmail.com>
2022-06-03 16:24:55 +0200

    update = big cut in code

--- DJ_assistant.py
+++ DJ_assistant.py
@@ -12,7 +12,10 @@ local_css("style.css")
 
 Types_of_Features = ("acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "tempo", "valence")
 
-st.title("DJ Assistant")
+
+st.markdown("<h1 style='text-align: center; color: white;'>DJ Assistant</h1>", unsafe_allow_html=True)
+
+#st.title("DJ Assistant")
 Name_of_Artist = st.text_input("Artist Name")
 Name_of_song = st.text_input("Song Name")
 Name_of_Feat = st.selectbox("Feature", Types_of_Features)
@@ -41,6 +44,20 @@ spotify = SpotifyAPI(client_id, client_secret)
 
 Data = spotify.search({"artist": f"{Name_of_Artist}", "track": f"{Name_of_song}"}, search_type="track")
 
+need = []
+for i, item in enumerate(Data['tracks']['items']):
+    track = item['album']
+    song_name = item['name']
+    track_uri = item["uri"]
+    #explicit = item['explicit']
+    need.append((i, track['artists'][0]['name'], song_name, track['release_date']))
+
+
+st.write(need[0][1])
+st.write(need[0][2])
+st.write(need[0][3])
+
+
 
 #st.write(Data)
 #Data = spotify.search({"track": f"{Name_of_song}"}, search_type="track")
@@ -48,17 +65,6 @@ Data = spotify.search({"artist": f"{Name_of_Artist}", "track": f"{Name_of_song}"
 #st.markdown(Data)
 
 #st.dataframe(data=Data, width=None, height=None)
-
-need = []
-for i, item in enumerate(Data['tracks']['items']):
-    track = item['album']
-    track_id = item['id']
-    song_name = item['name']
-    popularity = item['popularity']
-    track_uri = item["uri"]
-    #explicit = item['explicit']
-    need.append((i, track['artists'][0]['name'], track['name'], track_id, song_name, track['release_date'], popularity))
-    #need.append((i, track['artists'][0]['name'], track['name'], track_id, song_name, track['release_date'], popularity, explicit))
 
 
 #st.write(track) 
@@ -143,44 +149,6 @@ else:
 
 
 ##
-
-Track_df = pd.DataFrame(need, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity'))
-
-access_token = spotify.access_token
-
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-endpoint = "https://api.spotify.com/v1/audio-features/"
-
-Feat_df = pd.DataFrame()
-for id in Track_df['Id'].iteritems():
-    track_id = id[1]
-    lookup_url = f"{endpoint}{track_id}"
-    ra = requests.get(lookup_url, headers=headers)
-    audio_feat = ra.json()
-    Features_df = pd.DataFrame(audio_feat, index=[0])
-    Feat_df = Feat_df.append(Features_df)
-
-Full_Data = Track_df.merge(Feat_df, left_on="Id", right_on="id")
-
-Sort_DF = Full_Data.sort_values(by=['Popularity'], ascending=False)
-
-chart_df = Sort_DF[['Artist', 'Album Name', 'Song Name', 'Release Date', 'Popularity', f'{Name_of_Feat}']]
-
-# Import altair
-
-feat_header = Name_of_Feat.capitalize()
-
-st.header(f'{feat_header}' " vs. Popularity")
-c = alt.Chart(chart_df).mark_circle().encode(
-    alt.X('Popularity', scale=alt.Scale(zero=False)), y=f'{Name_of_Feat}', color=alt.Color('Popularity', scale=alt.Scale(zero=False)), 
-    size=alt.value(200), tooltip=['Popularity', f'{Name_of_Feat}', 'Song Name', 'Album Name'])
-
-st.altair_chart(c, use_container_width=True)
-
-st.header("Table of Attributes")
-st.table(chart_df)
 
 st.write("acousticness: Confidence measure from 0.0 to 1.0 on if a track is acoustic.")
 st.write("danceability: Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.")
